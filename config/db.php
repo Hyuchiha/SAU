e7dc0bd92931a7c88d4d2bd2635f535fd72582ef
Kev' Pacheco <kev.pachec.lis@gmail.com>
2015-10-03 23:44:22 -0500

    Agregado el proyecto con Yii, configuracion basica para iniciar el proyecto

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sau',
+    'username' => 'sau',
+    'password' => 'Sau2015',
+    'charset' => 'utf8',
+];
