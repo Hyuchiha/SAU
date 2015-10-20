1d1ad76c40afdec4106f6eface3891b5453608d4
Arian Castillo <caztle501@gmail.com>
2015-10-20 11:40:02 -0500

    Preparación de roles.

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -1,26 +1,47 @@
 <?php
 namespace app\commands;
-
 use Yii;
 use yii\console\Controller;
-
 class RbacController extends Controller
 {
-    public function actionInit()
+    public function actionCreateRoles()
     {
         $auth = Yii::$app->authManager;
-        $administrator = $auth->createRole('administrator');
+
+        // Creación de roles
+        $administrator = $auth->createRole('administrator');    // Administrador
+        $executive = $auth->createRole('executive');            // Directivo
+        $employeeArea = $auth->createRole('employeeArea');      // Empleado de área
+        $responsibleArea = $auth->createRole('responsibleArea');// Responsable de área
+        $requester = $auth->createRole('requester');            // Solicitante
+
+        // Guardado de roles en la base de datos
         $auth->add($administrator);
-        $auth->assign($administrator, 1);
+        $auth->add($executive);
+        $auth->add($employeeArea);
+        $auth->add($responsibleArea);
+        $auth->add($requester);
     }
-
-    public function actionSecond()
+    public function actionAsignRoles()
     {
+        /*
         $auth = Yii::$app->authManager;
-        $administrator = $auth->getRole('administrator');
-        $auth->assign($administrator, 2);
-    }
 
+        // Obtención de los roles
+        $administrator = $auth->getRole('administrator');
+        $executive = $auth->getRole('executive');
+        $employeeArea = $auth->getRole('employeeArea');
+        $responsibleArea = $auth->getRole('responsibleArea');
+        $requester = $auth->getRole('requester');
+
+        // Asignación de roles
+        $auth->assign($administrator, #);
+        $auth->assign($executive, #);
+        $auth->assign($employeeArea, #);
+        $auth->assign($responsibleArea, #);
+        $auth->assign($requester, #);
+*/
+    }
     public function actionThird()
     {
         $auth = Yii::$app->authManager;
